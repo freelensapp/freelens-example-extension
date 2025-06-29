1f1e0f695e914962fda1deceb7b1aa2ca6759a4a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-29 23:44:02 +0200

    Use functions rather than classes for React components

--- src/renderer/details/example-details.tsx
+++ src/renderer/details/example-details.tsx
@@ -1,5 +1,5 @@
 import { Renderer } from "@freelensapp/extensions";
-import React from "react";
+import { observer } from "mobx-react";
 import { Example } from "../k8s/example";
 import styleInline from "./example-details.scss?inline";
 
@@ -7,30 +7,17 @@ const {
   Component: { DrawerItem, MarkdownViewer },
 } = Renderer;
 
-interface ExampleDetailsState {
-  crds: Renderer.K8sApi.CustomResourceDefinition[];
-}
+export interface ExampleDetailsProps extends Renderer.Component.KubeObjectDetailsProps<Example> {}
 
-export class ExampleDetails extends React.Component<
-  Renderer.Component.KubeObjectDetailsProps<Example>,
-  ExampleDetailsState
-> {
-  public readonly state: Readonly<ExampleDetailsState> = {
-    crds: [],
-  };
-
-  render() {
-    const { object } = this.props;
-
-    return (
-      <>
-        <style>{styleInline}</style>
-        <div className="ExampleDetails">
-          <DrawerItem name="Description">
-            <MarkdownViewer markdown={object.spec.description ?? ""} />
-          </DrawerItem>
-        </div>
-      </>
-    );
-  }
-}
+export const ExampleDetails = observer(({ object }: ExampleDetailsProps) => {
+  return (
+    <>
+      <style>{styleInline}</style>
+      <div className="ExampleDetails">
+        <DrawerItem name="Description">
+          <MarkdownViewer markdown={object.spec.description ?? ""} />
+        </DrawerItem>
+      </div>
+    </>
+  );
+});
--- src/renderer/pages/examples-page.tsx
+++ src/renderer/pages/examples-page.tsx
@@ -1,6 +1,5 @@
 import { Common, Renderer } from "@freelensapp/extensions";
 import { observer } from "mobx-react";
-import React from "react";
 import { Link } from "react-router-dom";
 import { Example, type ExampleStore } from "../k8s/example";
 import styleInline from "./examples-page.scss?inline";
@@ -22,48 +21,45 @@ enum sortBy {
   age = "age",
 }
 
-@observer
-export class ExamplesPage extends React.Component<{ extension: Renderer.LensExtension }> {
-  render() {
-    const exampleApi = Renderer.K8sApi.apiManager.getApi(Example.apiBase);
-    const exampleStore = exampleApi && (Renderer.K8sApi.apiManager.getStore(exampleApi) as ExampleStore);
-    return (
-      exampleStore && (
-        <>
-          <style>{styleInline}</style>
-          <KubeObjectListLayout
-            tableId="exampleTable"
-            className="Examples"
-            store={exampleStore}
-            sortingCallbacks={{
-              [sortBy.name]: (example: Example) => example.getName(),
-              [sortBy.namespace]: (example: Example) => example.getNs(),
-              [sortBy.title]: (example: Example) => example.spec.title,
-              [sortBy.age]: (example: Example) => example.getCreationTimestamp(),
-            }}
-            searchFilters={[(example: Example) => example.getSearchFields()]}
-            renderHeaderTitle="Examples"
-            renderTableHeader={[
-              { title: "Name", className: "name", sortBy: sortBy.name },
-              { title: "Namespace", className: "namespace", sortBy: sortBy.namespace },
-              { title: "Title", className: "title", sortBy: sortBy.title },
-              { title: "Age", className: "age", sortBy: sortBy.age },
-            ]}
-            renderTableContents={(example: Example) => [
-              <WithTooltip>{example.getName()}</WithTooltip>,
-              <Link
-                key="link"
-                to={getDetailsUrl(namespacesApi.formatUrlForNotListing({ name: example.getNs() }))}
-                onClick={stopPropagation}
-              >
-                <WithTooltip>{example.getNs()}</WithTooltip>
-              </Link>,
-              <WithTooltip>{example.spec.title}</WithTooltip>,
-              <KubeObjectAge object={example} key="age" />,
-            ]}
-          />
-        </>
-      )
-    );
-  }
-}
+export const ExamplesPage = observer(() => {
+  const exampleApi = Renderer.K8sApi.apiManager.getApi(Example.apiBase);
+  const exampleStore = exampleApi && (Renderer.K8sApi.apiManager.getStore(exampleApi) as ExampleStore);
+  return exampleStore ? (
+    <>
+      <style>{styleInline}</style>
+      <KubeObjectListLayout
+        tableId="exampleTable"
+        className="Examples"
+        store={exampleStore}
+        sortingCallbacks={{
+          [sortBy.name]: (example: Example) => example.getName(),
+          [sortBy.namespace]: (example: Example) => example.getNs(),
+          [sortBy.title]: (example: Example) => example.spec.title,
+          [sortBy.age]: (example: Example) => example.getCreationTimestamp(),
+        }}
+        searchFilters={[(example: Example) => example.getSearchFields()]}
+        renderHeaderTitle="Examples"
+        renderTableHeader={[
+          { title: "Name", className: "name", sortBy: sortBy.name },
+          { title: "Namespace", className: "namespace", sortBy: sortBy.namespace },
+          { title: "Title", className: "title", sortBy: sortBy.title },
+          { title: "Age", className: "age", sortBy: sortBy.age },
+        ]}
+        renderTableContents={(example: Example) => [
+          <WithTooltip>{example.getName()}</WithTooltip>,
+          <Link
+            key="link"
+            to={getDetailsUrl(namespacesApi.formatUrlForNotListing({ name: example.getNs() }))}
+            onClick={stopPropagation}
+          >
+            <WithTooltip>{example.getNs()}</WithTooltip>
+          </Link>,
+          <WithTooltip>{example.spec.title}</WithTooltip>,
+          <KubeObjectAge object={example} key="age" />,
+        ]}
+      />
+    </>
+  ) : (
+    <></>
+  );
+});
