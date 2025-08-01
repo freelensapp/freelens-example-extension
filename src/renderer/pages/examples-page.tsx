f6da99114bb16f2081b583b3cfdf9564db4a96c4
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-08-01 15:23:06 +0200

    Typescript tweaks

--- src/renderer/pages/examples-page.tsx
+++ src/renderer/pages/examples-page.tsx
@@ -2,7 +2,7 @@ import { Common, Renderer } from "@freelensapp/extensions";
 import { observer } from "mobx-react";
 import { Link } from "react-router-dom";
 import { withErrorPage } from "../components/error-page";
-import { Example } from "../k8s/example";
+import { Example, type ExampleApi } from "../k8s/example";
 import styles from "./examples-page.module.scss";
 import stylesInline from "./examples-page.module.scss?inline";
 
@@ -19,36 +19,41 @@ const {
 const KubeObject = Example;
 type KubeObject = Example;
 
+const sortingCallbacks = {
+  name: (object: KubeObject) => object.getName(),
+  namespace: (object: KubeObject) => object.getNs(),
+  active: (object: KubeObject) => String(object.spec.active ?? false),
+  title: (object: KubeObject) => object.spec.title,
+  age: (object: KubeObject) => object.getCreationTimestamp(),
+};
+
+const renderTableHeader: { title: string; sortBy: keyof typeof sortingCallbacks; className?: string }[] = [
+  { title: "Name", sortBy: "name" },
+  { title: "Namespace", sortBy: "namespace" },
+  { title: "Active", sortBy: "active", className: styles.active },
+  { title: "Title", sortBy: "title", className: styles.title },
+  { title: "Age", sortBy: "age", className: styles.age },
+];
+
 export interface ExamplesPageProps {
   extension: Renderer.LensExtension;
 }
 
 export const ExamplesPage = observer((props: ExamplesPageProps) =>
   withErrorPage(props, () => {
-    const store = KubeObject.getStore();
+    const store = KubeObject.getStore<Example>();
+
     return (
       <>
         <style>{stylesInline}</style>
-        <KubeObjectListLayout
+        <KubeObjectListLayout<Example, ExampleApi>
           tableId={`${KubeObject.crd.singular}Table`}
           className={styles.examplesPage}
           store={store}
-          sortingCallbacks={{
-            name: (object: KubeObject) => object.getName(),
-            namespace: (object: KubeObject) => object.getNs(),
-            active: (object: KubeObject) => String(object.spec.active ?? false),
-            title: (object: KubeObject) => object.spec.title,
-            age: (object: KubeObject) => object.getCreationTimestamp(),
-          }}
+          sortingCallbacks={sortingCallbacks}
           searchFilters={[(object: KubeObject) => object.getSearchFields()]}
           renderHeaderTitle={KubeObject.crd.title}
-          renderTableHeader={[
-            { title: "Name", sortBy: "name" },
-            { title: "Namespace", sortBy: "namespace" },
-            { title: "Active", sortBy: "active", className: styles.active },
-            { title: "Title", sortBy: "title", className: styles.title },
-            { title: "Age", sortBy: "age", className: styles.age },
-          ]}
+          renderTableHeader={renderTableHeader}
           renderTableContents={(object: KubeObject) => [
             <WithTooltip>{object.getName()}</WithTooltip>,
             <Link
