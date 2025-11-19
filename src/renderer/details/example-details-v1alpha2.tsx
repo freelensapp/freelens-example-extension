4698db900237d65d7d7bdfe9cae6d51490543d8a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-11-19 19:02:36 +0100

    Return the page for available version

--- src/renderer/details/example-details-v1alpha2.tsx
+++ src/renderer/details/example-details-v1alpha2.tsx
@@ -2,8 +2,6 @@ import { Renderer } from "@freelensapp/extensions";
 import { observer } from "mobx-react";
 import { ExamplePreferencesStore } from "../../common/store";
 import { withErrorPage } from "../components/error-page";
-import styles from "./example-details.module.scss";
-import stylesInline from "./example-details.module.scss?inline";
 
 import type { Example } from "../k8s/example/example-v1alpha2";
 
@@ -22,16 +20,13 @@ export const ExampleDetails = observer((props: ExampleDetailsProps) =>
 
     return (
       <>
-        <style>{stylesInline}</style>
-        <div className={styles.exampleDetails}>
-          <DrawerItem name="Api Version">v1alpha2</DrawerItem>
-          <DrawerItem name="Description">
-            <MarkdownViewer markdown={object.spec.description ?? ""} />
-          </DrawerItem>
-          <DrawerItem name="Example checkbox">
-            <BadgeBoolean value={preferences.enabled} />
-          </DrawerItem>
-        </div>
+        <DrawerItem name="Api Version">v1alpha2</DrawerItem>
+        <DrawerItem name="Description">
+          <MarkdownViewer markdown={object.spec.description ?? ""} />
+        </DrawerItem>
+        <DrawerItem name="Example checkbox">
+          <BadgeBoolean value={preferences.enabled} />
+        </DrawerItem>
       </>
     );
   }),
